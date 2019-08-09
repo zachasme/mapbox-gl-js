e3573685f6b612fb41aef6d10d25e11cca5d00fe
ryanhamley <ryan.hamley@mapbox.com>
2019-08-09 13:11:43 -0700

    Fix lint issue

--- src/style-spec/validate/validate_number.js
+++ src/style-spec/validate/validate_number.js
@@ -8,6 +8,7 @@ export default function validateNumber(options) {
     const valueSpec = options.valueSpec;
     let type = getType(value);
 
+    // eslint-disable-next-line no-self-compare
     if (type === 'number' && value !== value) {
         type = 'NaN';
     }
